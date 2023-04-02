6b420186bfe8097a8b83c0200995c3b61f8181c2
Axawys <52900829+Axawys@users.noreply.github.com>
2023-04-02 20:23:36 +0300

    Added a forgotten bracket to the print in 1.py

--- 1/1.py
+++ 1/1.py
@@ -17,7 +17,7 @@ for x in range(2):
         for z in range(2):
             for w in range(2):
                 if not((x and not y) or (y == z) or not w):
-                    print(x, y, z, w
+                    print(x, y, z, w)
                           
 '''
 x y z w
